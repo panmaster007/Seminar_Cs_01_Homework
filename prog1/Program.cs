92fdf0085bbe60b49a42480a6789033b0cd56543
Igor <fullstack@naladka.su>
2023-01-27 19:19:36 +0300

    работа с 01

--- prog1/Program.cs
+++ prog1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int one = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int two = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(one);
